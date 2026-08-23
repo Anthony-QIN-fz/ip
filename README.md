abaf6a824ea74727f72170936d5272b13e34cf91
Qin <squ4reanthony@gmail.com>
2026-08-23 19:00:36 +0800

    Updated the code to echo commands entered by the user, and exit when the user types the command bye.

--- README.md
+++ README.md
@@ -19,11 +19,22 @@ Prerequisites: JDK 25, update Intellij to the most recent version.
     / _ \| | __ _ / _|
    | | | | |/ _` | |_
    | |_| | | (_| |  _|
-    \___/|_|\__,_|_|
+   \___/|_|\__,_|_|
 
    Hello! I'm Olaf. What can I do for you?
-
+   ____________________________________________________________
+   list
+   ____________________________________________________________
+   list
+   ____________________________________________________________
+   blah
+   ____________________________________________________________
+   blah
+   ____________________________________________________________
+   bye
+   ____________________________________________________________
    Bye. Hope to see you again soon!
+   ____________________________________________________________
    ```
 
 **Warning:** Keep the `src\main\java` folder as the root folder for Java files (i.e., don't rename those folders or move Java files to another folder outside of this folder path), as this is the default location some tools (e.g., Gradle) expect to find Java files.
--- src/main/java/Olaf.java
+++ src/main/java/Olaf.java
@@ -1,17 +1,64 @@
+import java.util.Scanner;
+
 /**
  * Runs the Olaf chatbot application.
  */
 public class Olaf {
+    private static final String BANNER = "  ___  _        __\n"
+            + " / _ \\| | __ _ / _|\n"
+            + "| | | | |/ _` | |_\n"
+            + "| |_| | | (_| |  _|\n"
+            + " \\___/|_|\\__,_|_|";
+    private static final String DIVIDER = "_".repeat(60);
+    private static final String GREETING = "Hello! I'm Olaf. What can I do for you?";
+    private static final String FAREWELL = "Bye. Hope to see you again soon!";
+    private static final String EXIT_COMMAND = "bye";
+
+    /**
+     * Starts Olaf's command loop.
+     *
+     * @param args command-line arguments, which Olaf does not use
+     */
     public static void main(String[] args) {
-        String banner = "  ___  _        __\n"
-                + " / _ \\| | __ _ / _|\n"
-                + "| | | | |/ _` | |_\n"
-                + "| |_| | | (_| |  _|\n"
-                + " \\___/|_|\\__,_|_|";
-        System.out.println(banner);
+        new Olaf().run();
+    }
+
+    private void run() {
+        printWelcome();
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                String command = scanner.nextLine();
+                if (isExitCommand(command)) {
+                    printFarewell();
+                    return;
+                }
+                echoCommand(command);
+            }
+        }
+    }
+
+    private void printWelcome() {
+        System.out.println(BANNER);
         System.out.println();
-        System.out.println("Hello! I'm Olaf. What can I do for you?");
+        System.out.println(GREETING);
+        System.out.println(DIVIDER);
+    }
+
+    private boolean isExitCommand(String command) {
+        return command.trim().equalsIgnoreCase(EXIT_COMMAND);
+    }
+
+    private void echoCommand(String command) {
+        System.out.println(DIVIDER);
         System.out.println();
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(command);
+        System.out.println(DIVIDER);
+    }
+
+    private void printFarewell() {
+        System.out.println(DIVIDER);
+        System.out.println();
+        System.out.println(FAREWELL);
+        System.out.println(DIVIDER);
     }
 }
